6ddb9025a816b681d407b26f5943700b8506e398
satya <narsss1234@gmail.com>
2023-10-15 19:00:45 +0530

    correction to  Q1

--- Q1.py
+++ Q1.py
@@ -4,27 +4,36 @@
 
 # importing regular regular expressions
 import re
-# create a variable that can store the input string which will provided by user
-password = str(input("Please enter a password: "))
+
 
 def check_password_strength(password1):
 
     # Check if the password has at least 8 characters
     if len(password1) < 8:
-        return "Entered password does not meet the length criteria: Minimum 8 characters"
+        return False
     # Check if the password contains at least one uppaercase letter
     elif not re.search(r'[A-Z]',password1):
-        return "Enterd password does not contain an uppercase letter."
+        return False
     # Check if the password contains at least one lowercase letter
     elif not re.search(r'[a-z]',password1):
-        return "Entered password does not contain a lowercase letter."
+        return False
     # Check if the password contains at least one digit
     elif not re.search(r'[1234567890]',password1):
-        return "Entered password does not contain a digit."
+        return False
     # Check if the password contains at least one special charcater
     elif not re.search(r'[!@#$%^&*()]',password1):
-        return "Enterd password does not contain a special charcater: allowed characters - !@#$%^&*()"
+        return False
     else:
-        return "Password has met the password policy criteria."
+        return True
     
-print(check_password_strength(password))
+# create a variable that can store the input string which will provided by user
+password = str(input("Please enter a password: "))
+
+if check_password_strength(password):
+    print("The entered password meets the password policy criteria.")
+else:
+    print("The password does not meet the criteria:\n")
+    print("○ Minimum length: The password should be at least 8 characters long.\n")
+    print("○ Contains both uppercase and lowercase letters.\n")
+    print("○ Contains at least one digit 0-9.\n")
+    print("○ Contains at least one special character !@#$%^&*()\n")
